36976c74738376b9ef8a737823ba3d64f2c5ee37
S4SPIDEY <41261098+S4SPIDEY@users.noreply.github.com>
2025-06-19 20:01:29 +0530

    Update aztec_setup.sh

--- aztec_setup.sh
+++ aztec_setup.sh
@@ -10,6 +10,8 @@ sudo apt install -y curl iptables build-essential git wget lz4 jq make gcc nano 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update && sudo apt install -y docker-ce
+
+
 sudo systemctl enable --now docker
 sudo usermod -aG docker $USER
 newgrp docker
